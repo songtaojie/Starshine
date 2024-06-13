fb3c56e5954fd5d277c0844d5482af6cd5f7e503
songtaojie <stjworkemail@163.com>
2024-06-13 18:31:35 +0800

    初始化项目

--- NetCore/Hx.Sdk.NetFramework.Autofac/DependencyInjection/IScopedDependency.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hx.Sdk.DependencyInjection
-{
-    /// <summary>
-    /// 每个请求一个实例
-    /// </summary>
-    public interface IScopedDependency
-    {
-    }
-}
--- NetCore/Hx.WebApi/Global.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Microsoft.AspNetCore.Builder;
-global using Microsoft.Extensions.DependencyInjection;
-global using NLog;
-global using NLog.Web;
-global using System;
-global using System.Configuration;
